bf4b3080075ed8cc989e875fae4dd07085039e21
emnshkw <menshikov_2004@list.ru>
2025-09-18 12:25:56 +0300

    1

--- tgserver/tgapi/serializers.py
+++ tgserver/tgapi/serializers.py
@@ -8,10 +8,18 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class DialogSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True, read_only=True)
+    last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Dialog
         fields = '__all__'
+        extra_fields = ["last_message"]
+
+    def get_last_message(self, obj):
+        last_msg = obj.messages.order_by("-date").first()
+        if last_msg:
+            return MessageSerializer(last_msg).data
+        return None
 class DialogCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dialog
