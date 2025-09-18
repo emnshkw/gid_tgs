7c7b39d22642ea658df2ab59b7d5756b66e75fe3
emnshkw <menshikov_2004@list.ru>
2025-09-18 17:39:57 +0300

    1

--- tgserver/tgserver/urls.py
+++ tgserver/tgserver/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from django.conf.urls.static import static,serve
 from django.conf import settings
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FLUTTER_WEB_APP = os.path.join(BASE_DIR, 'landing')
+
+def flutter_redirect(request, resource):
+    return serve(request, resource, FLUTTER_WEB_APP)
 
 
 urlpatterns = [
+    path('', lambda r: flutter_redirect(r, 'index.html')),
     path('admin/', admin.site.urls),
     path('', include('tgapi.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
