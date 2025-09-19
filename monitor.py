839178792d1bb71836a7954f8738449d1fa422d5
emnshkw <menshikov_2004@list.ru>
2025-09-19 12:10:44 +0300

    1

--- monitor.py
+++ monitor.py
@@ -116,9 +116,7 @@ def create_message(dialog_id, sender_name, text, date_iso, delivered=True, teleg
         "telegram_id": telegram_id
     }
     files = {'files':[]}
-    if media:
-        for f in media:
-            files['files'].append(f)
+    print(media)
     try:
         r = requests.post(f"{API_BASE}/messages/", json=payload,files=files)
         if r.status_code in (200, 201):
