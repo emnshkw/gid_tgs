5947c0fcf704e9eabe134677861b3e196ebe24d1
emnshkw <menshikov_2004@list.ru>
2025-09-22 11:11:27 +0300

    1

--- tgserver/tgapi/models.py
+++ tgserver/tgapi/models.py
@@ -38,6 +38,7 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=20, unique=True)
     session_name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f"Подключенный аккаунт - {self.phone_number} ({self.session_name})"
