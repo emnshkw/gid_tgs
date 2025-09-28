f622fd05a2ed17993fa808fa3dc97d6d4e04dbda
emnshkw <menshikov_2004@list.ru>
2025-09-28 16:13:59 +0300

    1

--- tgserver/ya_accounts_info/admin.py
+++ tgserver/ya_accounts_info/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import YaAccountModel
 
 
-admin.site.register(YaAccountModel)
+# ----- Actions -----
+@admin.action(description="Пометить как требующие обновления")
+def mark_need_update(modeladmin, request, queryset):
+    queryset.update(need_update=True)
+
+
+@admin.action(description="Снять пометку об обновлении")
+def unmark_need_update(modeladmin, request, queryset):
+    queryset.update(need_update=False)
+
+
+# ----- Admin -----
+@admin.register(YaAccountModel)
+class YaAccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "need_update")
+    list_filter = ("need_update", "city")
+    search_fields = ("name", "city")
+
+    actions = [mark_need_update, unmark_need_update]
