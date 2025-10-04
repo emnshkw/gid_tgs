e2de7dd758dac329d6e42ade473b1a53b6726116
emnshkw <menshikov_2004@list.ru>
2025-10-04 21:10:01 +0300

    1

--- monitor.py
+++ monitor.py
@@ -183,10 +183,8 @@ class TelegramWorker:
                 # Получаем сам диалог
                 async with self.http.get(f"{DJANGO_BASE}{DIALOGS_EP}{dialog_id}/", headers=django_headers()) as r:
                     dlg = await r.json()
-                dlg_list = await r.json()
-                if not dlg_list:
-                    continue
-                dlg = dlg_list[0]  # теперь это словарь
+
+                # dlg = dlg_list[0]  # теперь это словарь
                 phone = dlg['account_phone']
                 chat_id = dlg['chat_id']
                 print(chat_id)
