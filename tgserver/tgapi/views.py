1c189ce98dabee5182cd7bae2afe3364e1fd66f1
emnshkw <menshikov_2004@list.ru>
2025-10-07 13:02:03 +0300

    1

--- tgserver/tgapi/views.py
+++ tgserver/tgapi/views.py
@@ -66,7 +66,7 @@ class ProfilesAPIView(APIView):
             dialogs = requests.get('http://127.0.0.1:8001/api/dialogs/').json()
             for profile in profiles:
                 for d in dialogs:
-                    if d['account_phone'] == profile.phone_number:
+                    if d['account_phone'] == profile.phone_number and d['last_message']:
                         try:
                             if profile.last_message_date < parse_iso_datetime(d['last_message']['date']):
                                 profile.last_message_date = parse_iso_datetime(d['last_message']['date'])
