502d6b665c9d1dca9d047a0245c4e1b20a9a7f59
emnshkw <menshikov_2004@list.ru>
2025-10-07 12:51:12 +0300

    1

--- tgserver/tgapi/views.py
+++ tgserver/tgapi/views.py
@@ -69,26 +69,28 @@ class ProfilesAPIView(APIView):
                 return Response({"message": "Аккаунт не найден"})
         else:
             profiles = list(Profile.objects.all())
-            dialogs = requests.get('http://127.0.0.1:8001/api/dialogs/').json()
-            for i in range(len(profiles)):
-                for x in range(i+1,len(profiles)):
-                    first = profiles[i]
-                    second = profiles[x]
-                    first_dialogs_dates = []
-                    second_dialogs_dates = []
-                    for c in dialogs:
-                        if c['account_phone'] and c['account_phone'] == first.phone_number and c['last_message']:
-                            first_dialogs_dates.append(parse_iso_datetime(str(c['last_message']['date'])))
-                    for v in dialogs:
-                        if v['account_phone'] and v['account_phone'] == second.phone_number and v['last_message']:
-                            second_dialogs_dates.append(parse_iso_datetime(str(v['last_message']['date'])))
-                    first_dialogs_dates.sort()
-                    second_dialogs_dates.sort()
-                    if second_dialogs_dates[-1] > first_dialogs_dates[-1]:
-                        profiles[i] = second
-                        profiles[x] = first
-                        break
-            return Response(ProfileSelizalier(profiles,many=True).data)
+
+            return Response(ProfileSelizalier(profiles, many=True).data)
+            # dialogs = requests.get('http://127.0.0.1:8001/api/dialogs/').json()
+            # for i in range(len(profiles)):
+            #     for x in range(i+1,len(profiles)):
+            #         first = profiles[i]
+            #         second = profiles[x]
+            #         first_dialogs_dates = []
+            #         second_dialogs_dates = []
+            #         for c in dialogs:
+            #             if c['account_phone'] and c['account_phone'] == first.phone_number and c['last_message']:
+            #                 first_dialogs_dates.append(parse_iso_datetime(str(c['last_message']['date'])))
+            #         for v in dialogs:
+            #             if v['account_phone'] and v['account_phone'] == second.phone_number and v['last_message']:
+            #                 second_dialogs_dates.append(parse_iso_datetime(str(v['last_message']['date'])))
+            #         first_dialogs_dates.sort()
+            #         second_dialogs_dates.sort()
+            #         if second_dialogs_dates[-1] > first_dialogs_dates[-1]:
+            #             profiles[i] = second
+            #             profiles[x] = first
+            #             break
+            # return Response(ProfileSelizalier(profiles,many=True).data)
 
 
     def post(self,request,*args,**kwargs):
