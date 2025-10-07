0dc09fb5252d8fc7d6c4b8a6de805d7d0e2278dc
emnshkw <menshikov_2004@list.ru>
2025-10-07 12:21:30 +0300

    1

--- tgserver/tgapi/urls.py
+++ tgserver/tgapi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DialogListCreateView, MessageListCreateView, MessageUpdateDeliveredView,MessageMediaListCreateView, ProfileViewSet,MessagesBatchView
+from .views import DialogListCreateView, MessageListCreateView, MessageUpdateDeliveredView,MessageMediaListCreateView, ProfileViewSet,MessagesBatchView,ProfilesAPIView
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'api/profiles', ProfileViewSet)
