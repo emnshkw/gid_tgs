5176825061a78cc5c57173cbc31db19134eaf000
emnshkw <menshikov_2004@list.ru>
2025-10-07 12:42:23 +0300

    1

--- tgserver/tgapi/views.py
+++ tgserver/tgapi/views.py
@@ -64,8 +64,7 @@ class ProfilesAPIView(APIView):
         if pk:
             try:
                 data = ProfileSelizalier(Profile.objects.get(id=int(pk)))
-                return Response({'msg':data.data})
-                return Response(ProfileSelizalier(data).data)
+                return Response(data.data)
             except:
                 return Response({"message": "Аккаунт не найден"})
         else:
