3e9447089c873bd9be86f89fc9afe97713365b77
emnshkw <menshikov_2004@list.ru>
2025-10-14 13:04:35 +0300

    1

--- tgserver/tgserver/settings.py
+++ tgserver/tgserver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zh#wjf%744-d$4gckoh)$n&z@qf-emkf)f8hstj4=w449k6o60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
