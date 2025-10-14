85d6fcef684a2fb9834d98397b2178d7cc0988ac
emnshkw <menshikov_2004@list.ru>
2025-10-14 12:48:02 +0300

    1

--- tgserver/tgapi/models.py
+++ tgserver/tgapi/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Media(models.Model):
-    file = models.CharField(max_length=10000)
+    file = models.FileField(upload_to="media/")
     media_type = models.CharField(
         max_length=10000,
         choices=[
