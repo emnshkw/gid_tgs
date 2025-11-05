5c872c9aa24eb8d2f842f91d42ad128eedff3675
emnshkw <menshikov_2004@list.ru>
2025-11-05 09:41:50 +0300

    1

--- tgserver/ya_accounts_info/models.py
+++ tgserver/ya_accounts_info/models.py
@@ -15,6 +15,8 @@ class YaAccountModel(models.Model):
         need_update = 'Программа вносит изменения' if self.new_cats != '' or self.del_cats != '' or self.new_city != '' else "Аккаунт заполнен"
         new_cats_len = len([i for i in self.new_cats.replace("\r","").split("\n") if i != ''])
         del_cats_len = len([i for i in self.del_cats.replace("\r","").split("\n") if i != ''])
+        self.need_update = need_update
+        self.save()
         return f'{self.name}. {need_update}. {new_cats_len} категорий нужно добавить, {del_cats_len} категорий нужно удалить.'
 
 
