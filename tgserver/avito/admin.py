00a3953ff15727c2de7fb25427086fc0722ed6a6
emnshkw <menshikov_2004@list.ru>
2025-11-10 10:27:20 +0300

    1

--- tgserver/avito/admin.py
+++ tgserver/avito/admin.py
@@ -5,8 +5,8 @@ from .models import AvitoAd,AvitoAccount
 @admin.register(AvitoAd)
 class AvidoAdAdmin(admin.ModelAdmin):
     # list_display = ("name", "city", "need_update")
-    list_filter = ("updated_after_position")
-    search_fields = ("title")
+    list_filter = ("updated_after_position",)
+    search_fields = ("title",)
     actions = []
 # admin.site.register(AvitoAd)
 admin.site.register(AvitoAccount)
